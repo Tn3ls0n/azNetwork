e9c97fcb2c83fab8efe0904a76aad564e40c85af
Tn3ls0n <tyler.j.nelson@outlook.com>
2024-09-08 14:56:16 -0400

    Delete ResearchVnet directory

--- ResearchVnet/ResearchVnet.ps1
+++
@@ -1,5 +0,0 @@
-az network vnet create --resource-group "" --name ResearchVnet --address-prefixes 10.40.40.0/24 --location westindia
-
-az network vnet subnet create --resource-group "" --vnet-name ResearchVnet --name ResearchSystemSubnet --address-prefixes 10.40.40.0/24
-
-az network vnet subnet list --resource-group "" --vnet-name ResearchVnet --output table
