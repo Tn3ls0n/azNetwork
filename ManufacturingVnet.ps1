ec22b0d66e7b631c1c2266a70fd0324fd729957f
Tn3ls0n <tyler.j.nelson@outlook.com>
2024-09-08 15:04:36 -0400

    Update ManufacturingVnet.ps1

--- ManufacturingVnet.ps1
+++ ManufacturingVnet.ps1
@@ -1,4 +1,4 @@
-az network vnet        create --resource-group $rg --name ManufacturingVnet --address-prefixes 10.30.0.0/16 --location $manufacturingLocation
+az network vnet        create --resource-group $rg --name      ManufacturingVnet --address-prefixes 10.30.0.0/16 --location $manufacturingLocation
 az network vnet subnet create --resource-group $rg --vnet-name ManufacturingVnet --name ManufacturingSystemSubnet --address-prefixes 10.30.10.0/24
 az network vnet subnet create --resource-group $rg --vnet-name ManufacturingVnet --name SensorSubnet1 --address-prefixes 10.30.20.0/24
 az network vnet subnet create --resource-group $rg --vnet-name ManufacturingVnet --name SensorSubnet2 --address-prefixes 10.30.21.0/24
