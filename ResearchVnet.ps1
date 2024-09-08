ddbfdd00846025bf7f3e53c69b4c080ef7619cdc
Tn3ls0n <tyler.j.nelson@outlook.com>
2024-09-08 15:05:54 -0400

    Update ResearchVnet.ps1

--- ResearchVnet.ps1
+++ ResearchVnet.ps1
@@ -1,5 +1,3 @@
-az network vnet create --resource-group "" --name ResearchVnet --address-prefixes 10.40.40.0/24 --location westindia
-
-az network vnet subnet create --resource-group "" --vnet-name ResearchVnet --name ResearchSystemSubnet --address-prefixes 10.40.40.0/24
-
-az network vnet subnet list --resource-group "" --vnet-name ResearchVnet --output table
+az network vnet        create --resource-group $rg --name      ResearchVnet --address-prefixes 10.40.40.0/24 --location $researchLocation
+az network vnet subnet create --resource-group $rg --vnet-name ResearchVnet --name ResearchSystemSubnet --address-prefixes 10.40.40.0/24
+az network vnet subnet list   --resource-group $rg --vnet-name ResearchVnet --output table
