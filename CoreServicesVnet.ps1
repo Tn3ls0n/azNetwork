6e7a4b16cdfeec908c50605f91912cbe4417a231
Tn3ls0n <tyler.j.nelson@outlook.com>
2024-09-08 15:13:15 -0400

    Update CoreServicesVnet.ps1

--- CoreServicesVnet.ps1
+++ CoreServicesVnet.ps1
@@ -1,6 +1,6 @@
 az network vnet        create --resource-group $rg --name      CoreServicesVnet --address-prefixes 10.20.0.0/16 --location $coreServicesLocation
-az network vnet subnet create --resource-group $rg --vnet-name CoreServicesVnet --name GatewaySubnet          --address-prefixes 10.20.0.0/27
-az network vnet subnet create --resource-group $rg --vnet-name CoreServicesVnet --name SharedServicesSubnet   --address-prefixes 10.20.10.0/24
-az network vnet subnet create --resource-group $rg --vnet-name CoreServicesVnet --name DatabaseSubnet         --address-prefixes 10.20.20.0/24
-az network vnet subnet create --resource-group $rg --vnet-name CoreServicesVnet --name PublicWebServiceSubnet --address-prefixes 10.20.30.0/24
+az network vnet subnet create --resource-group $rg --vnet-name CoreServicesVnet --name GatewaySubnet            --address-prefixes 10.20.0.0/27
+az network vnet subnet create --resource-group $rg --vnet-name CoreServicesVnet --name SharedServicesSubnet     --address-prefixes 10.20.10.0/24
+az network vnet subnet create --resource-group $rg --vnet-name CoreServicesVnet --name DatabaseSubnet           --address-prefixes 10.20.20.0/24
+az network vnet subnet create --resource-group $rg --vnet-name CoreServicesVnet --name PublicWebServiceSubnet   --address-prefixes 10.20.30.0/24
 az network vnet subnet list   --resource-group $rg --vnet-name CoreServicesVnet --output table
