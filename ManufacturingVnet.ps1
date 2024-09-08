d88c5f74af7bcf6eb9d90756a2d912ebae0e2f0c
Tn3ls0n <tyler.j.nelson@outlook.com>
2024-09-08 15:03:46 -0400

    Update ManufacturingVnet.ps1

--- ManufacturingVnet.ps1
+++ ManufacturingVnet.ps1
@@ -1,11 +1,6 @@
-az network vnet create --resource-group "" --name ManufacturingVnet --address-prefixes 10.30.0.0/16 --location northeurope
-
-az network vnet subnet create --resource-group "" --vnet-name ManufacturingVnet --name ManufacturingSystemSubnet --address-prefixes 10.30.10.0/24
-
-az network vnet subnet create --resource-group "" --vnet-name ManufacturingVnet --name SensorSubnet1 --address-prefixes 10.30.20.0/24
-
-az network vnet subnet create --resource-group "" --vnet-name ManufacturingVnet --name SensorSubnet2 --address-prefixes 10.30.21.0/24
-
-az network vnet subnet create --resource-group "" --vnet-name ManufacturingVnet --name SensorSubnet3 --address-prefixes 10.30.22.0/24
-
-az network vnet subnet list --resource-group "" --vnet-name ManufacturingVnet --output table
+az network vnet        create --resource-group $rg --name ManufacturingVnet --address-prefixes 10.30.0.0/16 --location $manufacturingLocation
+az network vnet subnet create --resource-group $rg --vnet-name ManufacturingVnet --name ManufacturingSystemSubnet --address-prefixes 10.30.10.0/24
+az network vnet subnet create --resource-group $rg --vnet-name ManufacturingVnet --name SensorSubnet1 --address-prefixes 10.30.20.0/24
+az network vnet subnet create --resource-group $rg --vnet-name ManufacturingVnet --name SensorSubnet2 --address-prefixes 10.30.21.0/24
+az network vnet subnet create --resource-group $rg --vnet-name ManufacturingVnet --name SensorSubnet3 --address-prefixes 10.30.22.0/24
+az network vnet subnet list   --resource-group $rg --vnet-name ManufacturingVnet --output table
