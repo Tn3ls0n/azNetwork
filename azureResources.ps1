0414f420d9dc37330be099552d8a7ce42872e162
Tn3ls0n <tyler.j.nelson@outlook.com>
2024-09-08 21:29:08 -0400

    Update azureResources.ps1

--- azureResources.ps1
+++ azureResources.ps1
@@ -21,3 +21,13 @@ az network vnet create --resource-group $rg --name  ResearchVnet --address-prefi
 az network vnet subnet create --resource-group $rg --vnet-name ResearchVnet --name appSubnet --address-prefixes 10.40.40.0/24
 az network vnet subnet list --resource-group $rg --vnet-name ResearchVnet --output table
 az vm create --resource-group $rg --no-wait --name ResearchVM --location westeurope --vnet-name ResearchVNet --subnet appSubnet --image Ubuntu2204 --admin-username $adminUsername --admin-password $adminPassword
+
+# list vnets...
+az network vnet list --query "[?contains(provisioningState, 'Succeeded')]" --output table
+
+# list vms...
+watch -d -n 5 "az vm list \
+    --resource-group $rg \
+    --show-details \
+    --query '[*].{Name:name, ProvisioningState:provisioningState, PowerState:powerState}' \
+    --output table"
