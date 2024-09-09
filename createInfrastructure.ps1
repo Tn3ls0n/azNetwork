e6df9d30a00609dd7a7e658959845516210fa81e
Tn3ls0n <tyler.j.nelson@outlook.com>
2024-09-08 21:14:59 -0400

    Update and rename createNetwork.ps1 to createInfrastructure.ps1

--- createInfrastructure.ps1
+++ createInfrastructure.ps1
@@ -1,9 +1,19 @@
 az login
+
 $rg = Read-Host "Enter rg name"
 $coreServicesLocation  = Read-Host "Enter core services vnet location" 
 $manufacturingLocation = Read-Host "Enter manufacturing vnet location"
 $researchLocation = Read-Host "Enter reseach vnet location"
+$adminUsername = Read-Host "Enter admin username" -AsSecureString
+$adminPassword = Read-Host "Enter admin password" -AsSecureString
+
 .\CoreServices.ps1 --no-wait
 .\Manufacturing.ps1 --no-wait
 .\Research.ps1 --no-wait
 az network vnet list --query "[?contains(provisioningState, 'Succeeded')]" --output table
+
+watch -d -n 5 "az vm list \
+    --resource-group $rg \
+    --show-details \
+    --query '[*].{Name:name, ProvisioningState:provisioningState, PowerState:powerState}' \
+    --output table"
