71a27962d972419ddcfaeb2ba504ed32cb9aaabb
Tn3ls0n <tyler.j.nelson@outlook.com>
2024-09-08 21:28:10 -0400

    Update createInfrastructure.ps1

--- createInfrastructure.ps1
+++ createInfrastructure.ps1
@@ -7,15 +7,6 @@ $researchLocation = Read-Host "Enter reseach vnet location"
 $adminUsername = Read-Host "Enter admin username" -AsSecureString
 $adminPassword = Read-Host "Enter admin password" -AsSecureString
 
-.\CoreServices.ps1 --no-wait
-.\Manufacturing.ps1 --no-wait
-.\Research.ps1 --no-wait
-
-az network vnet list --query "[?contains(provisioningState, 'Succeeded')]" --output table
-watch -d -n 5 "az vm list \
-    --resource-group $rg \
-    --show-details \
-    --query '[*].{Name:name, ProvisioningState:provisioningState, PowerState:powerState}' \
-    --output table"
+.\azureResources.ps1 
 
 .\peerings.ps1
