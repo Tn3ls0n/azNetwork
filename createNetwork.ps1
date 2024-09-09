2d0c22ef563b54f5abe71d735bd6c2bed696b724
Tn3ls0n <tyler.j.nelson@outlook.com>
2024-09-08 21:03:27 -0400

    Update createNetwork.ps1

--- createNetwork.ps1
+++ createNetwork.ps1
@@ -1,8 +1,9 @@
 az login
-$rg                    = Read-Host "Enter rg name"
+$rg = Read-Host "Enter rg name"
 $coreServicesLocation  = Read-Host "Enter core services vnet location" 
 $manufacturingLocation = Read-Host "Enter manufacturing vnet location"
-$researchLocation      = Read-Host "Enter reseach vnet location"
-.\CoreServicesVnet.ps1  --no-wait
-.\ManufacturingVnet.ps1 --no-wait
-.\ResearchVnet.ps1      --no-wait
+$researchLocation = Read-Host "Enter reseach vnet location"
+.\CoreServices.ps1 --no-wait
+.\Manufacturing.ps1 --no-wait
+.\Research.ps1 --no-wait
+az network vnet list --query "[?contains(provisioningState, 'Succeeded')]" --output table
