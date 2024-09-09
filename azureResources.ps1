cc23deda1907f5b720303b728e4c582895fec716
Tn3ls0n <tyler.j.nelson@outlook.com>
2024-09-08 21:26:33 -0400

    Update and rename CoreServices.ps1 to azureResources.ps1

--- azureResources.ps1
+++ azureResources.ps1
@@ -15,3 +15,9 @@ az network vnet subnet create --resource-group $rg --vnet-name ManufacturingVnet
 az network vnet subnet create --resource-group $rg --vnet-name ManufacturingVnet --name SensorSubnet1 --address-prefixes 10.30.22.0/24
 az network vnet subnet list --resource-group $rg --vnet-name ManufacturingVnet --output table
 az vm create --resource-group $rg --no-wait --name MarketingVM --location northeurope --vnet-name ManufacturingVnet --subnet appSubnet --image Ubuntu2204 --admin-username $adminUsername --admin-password $adminPassword
+
+# research infrastructure...
+az network vnet create --resource-group $rg --name  ResearchVnet --address-prefixes 10.40.40.0/24 --location $researchLocation
+az network vnet subnet create --resource-group $rg --vnet-name ResearchVnet --name appSubnet --address-prefixes 10.40.40.0/24
+az network vnet subnet list --resource-group $rg --vnet-name ResearchVnet --output table
+az vm create --resource-group $rg --no-wait --name ResearchVM --location westeurope --vnet-name ResearchVNet --subnet appSubnet --image Ubuntu2204 --admin-username $adminUsername --admin-password $adminPassword
