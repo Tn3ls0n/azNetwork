46777b2618650a44d90f8522657e46d9968a3abc
Tn3ls0n <tyler.j.nelson@outlook.com>
2024-09-08 21:32:43 -0400

    Update and rename peering.ps1 to peerings.ps1

--- peerings.ps1
+++ peerings.ps1
@@ -1,9 +1,17 @@
+# create sales and marketing vnet peering...
 az network vnet peering create --name SalesVNet-To-MarketingVNet --remote-vnet MarketingVNet --resource-group $rg --vnet-name SalesVNet --allow-vnet-access
 az network vnet peering create --name MarketingVNet-To-SalesVNet --remote-vnet SalesVNet --resource-group $rg --vnet-name MarketingVNet --allow-vnet-access
 
+# create reseach and marketing vnet peering...
 az network vnet peering create --name MarketingVNet-To-ResearchVNet --remote-vnet ResearchVNet --resource-group $rg --vnet-name MarketingVNet --allow-vnet-access
 az network vnet peering create --name ResearchVNet-To-MarketingVNet --remote-vnet MarketingVNet --resource-group $rg --vnet-name ResearchVNet --allow-vnet-access
 
+# list peerings for vnets...
 az network vnet peering list --resource-group $rg --vnet-name SalesVNet --query "[].{Name:name, Resource:resourceGroup, PeeringState:peeringState, AllowVnetAccess:allowVirtualNetworkAccess}" --output table
 az network vnet peering list --resource-group $rg --vnet-name ResearchVNet --query "[].{Name:name, Resource:resourceGroup, PeeringState:peeringState, AllowVnetAccess:allowVirtualNetworkAccess}" --output table
 az network vnet peering list --resource-group $rg --vnet-name MarketingVNet --query "[].{Name:name, Resource:resourceGroup, PeeringState:peeringState, AllowVnetAccess:allowVirtualNetworkAccess}" --output table
+
+# list effective routes...
+az network nic show-effective-route-table --resource-group $rg --name SalesVMVMNic --output table
+az network nic show-effective-route-table --resource-group $rg --name MarketingVMVMNic --output table
+az network nic show-effective-route-table --resource-group $rg --name ResearchVMVMNic --output table
